d4dcc36f407941aa4ea0ce0a0dd41fcb3b0fd260
Rui Abreu Ferreira <raf-ep@gmx.com>
2011-08-18 00:26:08 +0100

    Initial support clientserver
    - This brings in placeholders for clientserver support
    * It is NOT working yet it just prints debug messages

--- src/eval.c
+++ src/eval.c
@@ -15038,7 +15038,7 @@ remote_common(argvars, rettv, expr)
     char_u	*keys;
     char_u	*r = NULL;
     char_u	buf[NUMBUFLEN];
-# ifdef WIN32
+# if defined(WIN32) || defined(FEAT_GUI_QT)
     HWND	w;
 # else
     Window	w;
@@ -15056,7 +15056,7 @@ remote_common(argvars, rettv, expr)
     if (server_name == NULL)
 	return;		/* type error; errmsg already given */
     keys = get_tv_string_buf(&argvars[1], buf);
-# ifdef WIN32
+# if defined(WIN32) || defined(FEAT_GUI_QT)
     if (serverSendToVim(server_name, keys, &r, &w, expr, TRUE) < 0)
 # else
     if (serverSendToVim(X_DISPLAY, server_name, keys, &r, &w, expr, 0, TRUE)
@@ -15140,7 +15140,7 @@ f_remote_peek(argvars, rettv)
 #ifdef FEAT_CLIENTSERVER
     dictitem_T	v;
     char_u	*s = NULL;
-# ifdef WIN32
+# if defined(WIN32) || defined(FEAT_GUI_QT)
     long_u	n = 0;
 # endif
     char_u	*serverid;
@@ -15156,7 +15156,7 @@ f_remote_peek(argvars, rettv)
 	rettv->vval.v_number = -1;
 	return;		/* type error; errmsg already given */
     }
-# ifdef WIN32
+# if defined(WIN32) || defined(FEAT_GUI_QT)
     sscanf(serverid, SCANF_HEX_LONG_U, &n);
     if (n == 0)
 	rettv->vval.v_number = -1;
@@ -15201,7 +15201,7 @@ f_remote_read(argvars, rettv)
 
     if (serverid != NULL && !check_restricted() && !check_secure())
     {
-# ifdef WIN32
+# if defined(WIN32) || defined(FEAT_GUI_QT)
 	/* The server's HWND is encoded in the 'id' parameter */
 	long_u		n = 0;
 
@@ -16299,7 +16299,7 @@ f_serverlist(argvars, rettv)
     char_u	*r = NULL;
 
 #ifdef FEAT_CLIENTSERVER
-# ifdef WIN32
+# if defined(WIN32) || defined(FEAT_GUI_QT)
     r = serverGetVimNames();
 # else
     make_connection();
--- src/main.c
+++ src/main.c
@@ -3539,7 +3539,7 @@ exec_on_server(parmp)
 {
     if (parmp->serverName_arg == NULL || *parmp->serverName_arg != NUL)
     {
-# ifdef WIN32
+# if defined(WIN32) || defined(FEAT_GUI_QT)
 	/* Initialise the client/server messaging infrastructure. */
 	serverInitMessaging();
 # endif
@@ -3563,7 +3563,7 @@ exec_on_server(parmp)
 	 * clipboard first, it's further down. */
 	parmp->servername = serverMakeName(parmp->serverName_arg,
 							      parmp->argv[0]);
-# ifdef WIN32
+# if defined(WIN32) || defined(FEAT_GUI_QT)
 	if (parmp->servername != NULL)
 	{
 	    serverSetName(parmp->servername);
@@ -3797,7 +3797,7 @@ cmdsrv_main(argc, argv, serverName_arg, serverStr)
 		vim_memset(done, 0, numFiles);
 		while (memchr(done, 0, numFiles) != NULL)
 		{
-# ifdef WIN32
+# if defined(WIN32) || defined(FEAT_GUI_QT)
 		    p = serverGetReply(srv, NULL, TRUE, TRUE);
 		    if (p == NULL)
 			break;
@@ -3826,7 +3826,7 @@ cmdsrv_main(argc, argv, serverName_arg, serverStr)
 	{
 	    if (i == *argc - 1)
 		mainerr_arg_missing((char_u *)argv[i]);
-# ifdef WIN32
+# if defined(WIN32) || defined(FEAT_GUI_QT)
 	    /* Win32 always works? */
 	    if (serverSendToVim(sname, (char_u *)argv[i + 1],
 						    &res, NULL, 1, FALSE) < 0)
@@ -3849,7 +3849,7 @@ cmdsrv_main(argc, argv, serverName_arg, serverStr)
 	}
 	else if (STRICMP(argv[i], "--serverlist") == 0)
 	{
-# ifdef WIN32
+# if defined(WIN32) || defined(FEAT_GUI_QT)
 	    /* Win32 always works? */
 	    res = serverGetVimNames();
 # else
